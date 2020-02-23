9091442e1aa6269d216f538aaf38afa92ae91653
dinoxas <dhassiotis07@gmail.com>
2020-02-23 16:02:28 +0000

    Created Nav and Footer static components

---
+++ src/components/Nav.js
@@ -0,0 +1,28 @@
+import React, { Fragment } from "react";
+import { FaImdb, FaReact } from "react-icons/fa";
+
+function Nav() {
+  return (
+    <Fragment>
+      <nav className="navbar navbar-light bg-dark mb-5">
+        <div className="container">
+          <div className="navbar-header">
+            <span className="navbar-brand text-white text-lg brand-text">
+              Movies &amp; TV Series App
+            </span>
+          </div>
+          <ul className="navbar-nav ml-auto text-light d-inline-block">
+            <li className="nav-item d-inline-block mr-4">
+              <FaImdb color="#f5de50" size="60" />
+            </li>
+            <li className="nav-item d-inline-block mr-4">
+              <FaReact color="#00d8ff" size="60" />
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </Fragment>
+  );
+}
+
+export default Nav;
